86cd9cd99dc6d0403d7ddc5f4d28184af82a40ce
johnsonrobot <johnson860312@gmail.com>
2021-07-31 22:47:31 +0800

    s3 upload in bucket name

--- upload.py
+++ upload.py
@@ -1,4 +1,19 @@
 import boto3
 
+bucket_name = ['johnson-test-s3', 'johnsonelblog', 'johnsontestendpoint']
+
+file_data = open("test.txt", "rb").read()
+
 client = boto3.client('s3')
-print(client.list_buckets())
+# # for i in range(len(client.list_buckets().get('Buckets'))):
+# #     print(client.list_buckets().get('Buckets')[i])
+
+# # response = client.list_objects(Bucket = bucket_name[0])['Contents']
+# # for i in response:
+# #     print(i['Key'])
+
+response = client.put_object(
+    Bucket=bucket_name[0],
+    Body=file_data,
+    Key="APItest/test.txt"
+)
