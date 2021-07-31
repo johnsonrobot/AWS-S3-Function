64d8047995471d8a34667c5fe6e72c46ad2bb474
johnsonrobot <johnson860312@gmail.com>
2021-08-01 04:14:09 +0800

    modify upload function

--- upload.py
+++ upload.py
@@ -1,26 +1,35 @@
 import boto3
 
 bucket_name = ['johnson-test-s3', 'johnsonelblog', 'johnsontestendpoint']
+file_name = "test.txt"
+Path = "APItest/"
 
-file_data = open("test.txt", "rb").read()
+# file_data = open("test.txt", "rb").read()
 class S3():
     def __init__(self):
         self.client = boto3.client('s3')
         self.buckets_str = "Buckets"
         self.buckets_name = "Name"
+        self.object_content = "Contents"
+        self.object_key = "Key"
 
     def list_buckets(self):
-        super().__init__()
         buckets = self.client.list_buckets().get(self.buckets_str)
         for num in range(len(buckets)):
             print(buckets[num][self.buckets_name])
 
-    def list_objects(self):
-        super().__init__()
-        
-# # response = client.list_objects(Bucket = bucket_name[0])['Contents']
-# # for i in response:
-# #     print(i['Key'])
+    def list_objects(self, bucket_name):
+        objects = self.client.list_objects(Bucket = bucket_name)[self.object_content]
+        for object in objects:
+            print(object[self.object_key])
+
+    def put_object(self, bucket_name, file_data, key):
+        self.client.put_object(
+            Bucket = bucket_name,
+            Body = file_data,
+            Key = key
+        )
+        print("Bucket: {}\n Content: {}\n Path: {}" .format(bucket_name, file_data, key))
 
 # response = client.put_object(
 #     Bucket=bucket_name[0],
@@ -29,5 +38,9 @@ class S3():
 # )
 
 if __name__ == '__main__':
-    S3().list_buckets()
+    with open(file_name, "r") as f:
+        data = f.read()
+    S3().put_object(bucket_name[0], data, Path + file_name)
+    # S3().list_buckets()
+    # S3().list_objects(bucket_name[0])
     # print(a)
