0ecb60b8daad2434c7c91e1654b797fae047ced9
johnsonrobot <johnson860312@gmail.com>
2021-08-01 15:17:48 +0800

    s3 multipal files upload

--- upload.py
+++ upload.py
@@ -1,9 +1,11 @@
 import boto3
+from os import listdir
 
 bucket_name = ['johnson-test-s3', 'johnsonelblog', 'johnsontestendpoint']
-file_name = "test.txt"
+# file_name = "test.txt"
 Path = "APItest/"
-
+file_path = "file/"
+data = []
 # file_data = open("test.txt", "rb").read()
 class S3():
     def __init__(self):
@@ -23,24 +25,43 @@ class S3():
         for object in objects:
             print(object[self.object_key])
 
-    def put_object(self, bucket_name, file_data, key):
-        self.client.put_object(
-            Bucket = bucket_name,
-            Body = file_data,
-            Key = key
-        )
-        print("Bucket: {}\n Content: {}\n Path: {}" .format(bucket_name, file_data, key))
+    def put_object(self, bucket_name, file_data, key_path, key_file):
+        if len(key_file) > 1:
+            for i in range(len(key_file)):
+                self.client.put_object(
+                    Bucket = bucket_name,
+                    Body = file_data[i],
+                    Key = key_path + key_file[i]
+                )
+                print("No.i\nBucket: {}\n Content: {}\n Path: {}" .format(i, bucket_name, file_data[i], key_path + key_file[i]))
 
 # response = client.put_object(
 #     Bucket=bucket_name[0],
 #     Body=file_data,
 #     Key="APItest/test.txt"
 # )
+def files(fpath):
+    return listdir(fpath)
+
+def read_files(f, path):
+    if len(f) > 1:
+        fd = []
+        for num in range(len(f)):
+            with open(path + f[num], "r") as ff:
+                fd.append(ff.read())
+        return fd
+    else:
+        with open(path + f[-1], "r") as ff:
+            return ff.read()
 
 if __name__ == '__main__':
-    with open(file_name, "r") as f:
-        data = f.read()
-    S3().put_object(bucket_name[0], data, Path + file_name)
+    # with open(file_name, "r") as f:
+    #     data = f.read()
+    file = files(file_path)
+    body = read_files(file, file_path)
+    # print(data)
+    # print(file)
+    S3().put_object(bucket_name[0], body, Path, file)
     # S3().list_buckets()
     # S3().list_objects(bucket_name[0])
     # print(a)
